75d2b8bd6a22812006db4021257f38166ef1ef18
Your Name <you@example.com>
2014-07-07 10:58:20 +0800

    add

--- src/main/webapp/static/js/userRoleList.js
+++ src/main/webapp/static/js/userRoleList.js
@@ -13,92 +13,94 @@ var userRoleListLoader = function () {
 
     CommonUtil.beforeLoad();
     var c = new Command("role", "userRoleList", {});
-
+    var simpleRoleCollection = new Backbone.Collection;
     CommonUtil.doPost(c, function (msg) {
-            var simpleRoleCollection = new Backbone.Collection;
-            // 加载数据到collection中
-            _.each(msg.data.list, function (userRole, index, list) {
-                var simpleRoleModel = new Backbone.Model({
-                    roleName: userRole.roleName,
-                    fightForce: userRole.fightForce,
-                    attack: userRole.attack,
-                    defence: userRole.defence,
-                    health: userRole.health,
-                    level: userRole.level,
-                    id: userRole.id,
-                    armor: userRole.armor,
-                    accessory: userRole.accessory,
-                    weapon: userRole.weapon,
-                    armorInfo: userRole.weaponInfo,
-                    accessoryInfo: userRole.accessoryInfo,
-                    weaponInfo: userRole.weaponInfo
+
+        // 加载数据到collection中
+        _.each(msg.data.list, function (userRole, index, list) {
+            var simpleRoleModel = new Backbone.Model({
+                roleName: userRole.roleName,
+                fightForce: userRole.fightForce,
+                attack: userRole.attack,
+                defence: userRole.defence,
+                health: userRole.health,
+                level: userRole.level,
+                id: userRole.id,
+                armor: userRole.armor,
+                accessory: userRole.accessory,
+                weapon: userRole.weapon,
+                armorInfo: userRole.weaponInfo,
+                accessoryInfo: userRole.accessoryInfo,
+                weaponInfo: userRole.weaponInfo
+            });
+            simpleRoleCollection.add(simpleRoleModel);
+
+        });
+    });
+
+    var RoleListView = Backbone.View.extend({
+        el: "#roleList",
+        template: _.template($("#roleRowTemplate").html()),
+        template2: _.template($("#roleDetailTemplate").html()),
+
+
+        render: function () {
+            for (var m  in this.model) {
+                this.eachModel(this.model[m]);
+            }
+            this.showRoleDetail({data: {role: this.model[0].toJSON()}});
+            return this;
+        },
+        eachModel: function (data) {
+            $(this.el).append(this.template(data.toJSON()));
+            $("#showRoleDetail" + data.toJSON().id).on("click", {role: data.toJSON()}, this.show2);
+        },
+        show2: function (event) {
+            roleListView.showRoleDetail(event);
+        },
+        showRoleDetail: function (event) {
+            $("#roleDetail").html("");
+            var role = event.data.role;
+            var t = this.template2({
+                roleName: role.roleName,
+                fightForce: role.fightForce,
+                attack: role.attack,
+                defence: role.defence,
+                health: role.health,
+                level: role.level,
+                id: role.id,
+                weapon: role.weapon,
+                accessory: role.accessory,
+                armor: role.armor,
+                NO_WEAPON: roleListParams.NO_WEAPON,
+                NO_ACCESSORY: roleListParams.NO_ACCESSORY,
+                NO_ARMOR: roleListParams.NO_ARMOR
+
+            });
+            $("#roleDetail").html(t);
+            $("#weapon" + role.id).on("click", {id: role.weapon, urId: role.id, type: "weapon"}, this.nav2EquipDetail);
+            $("#accessory" + role.id).on("click", {id: role.accessory, urId: role.id, type: "accessory"}, this.nav2EquipDetail);
+            $("#armor" + role.id).on("click", {id: role.armor, urId: role.id, type: "armor"}, this.nav2EquipDetail);
+
+        },
+        nav2EquipDetail: function (event) {
+            if (event.data.id == 0) {
+                CommonUtil.nav2Url("wearEquip.html", {
+                    ueId: event.data.id,
+                    type: event.data.type,
+                    urId: event.data.urId
                 });
-                simpleRoleCollection.add(simpleRoleModel);
+            } else {
+                CommonUtil.nav2Url("equipDetail.html", {
+                    ueId: event.data.id,
+                    type: event.data.type,
+                    urId: event.data.urId
+                });
+            }
 
-            });
-
-            var RoleListView = Backbone.View.extend({
-                el: "#roleList",
-                template: _.template($("#roleRowTemplate").html()),
-
-
-                render: function () {
-                    for (var m  in this.model) {
-                        this.eachModel(this.model[m]);
-                    }
-                    this.showRoleDetail({data: {role: this.model[0].toJSON()}});
-                    return this;
-                },
-                eachModel: function (data) {
-                    $(this.el).append(this.template(data.toJSON()));
-                    $("#showRoleDetail" + data.toJSON().id).on("click", {role: data.toJSON()}, this.showRoleDetail);
-                },
-                showRoleDetail: function (event) {
-                    var role = event.data.role;
-                    var roleDetailHtml = $("#roleDetailTemplate").html();
-                    var t = _.template(roleDetailHtml, {
-                        roleName: role.roleName,
-                        fightForce: role.fightForce,
-                        attack: role.attack,
-                        defence: role.defence,
-                        health: role.health,
-                        level: role.level,
-                        id: role.id,
-                        weapon: role.weapon,
-                        accessory: role.accessory,
-                        armor: role.armor,
-                        NO_WEAPON: roleListParams.NO_WEAPON,
-                        NO_ACCESSORY: roleListParams.NO_ACCESSORY,
-                        NO_ARMOR: roleListParams.NO_ARMOR
-
-                    });
-                    $("#roleDetail").html(t);
-                    $("#weapon").on("click", {id: role.weapon, urId: role.id, type: "weapon"}, this.nav2EquipDetail);
-                    $("#accessory").on("click", {id: role.accessory, urId: role.id, type: "accessory"}, this.nav2EquipDetail);
-                    $("#armor").on("click", {id: role.armor, urId: role.id, type: "armor"}, this.nav2EquipDetail);
-
-                },
-                nav2EquipDetail: function (event) {
-                    if (event.data.id == 0) {
-                        CommonUtil.nav2Url("wearEquip.html", {
-                            ueId: event.data.id,
-                            type: event.data.type,
-                            urId: event.data.urId
-                        });
-                    } else {
-                        CommonUtil.nav2Url("equipDetail.html", {
-                            ueId: event.data.id,
-                            type: event.data.type,
-                            urId: event.data.urId
-                        });
-                    }
-
-                }
-            });
-
-            var roleListView = new RoleListView({model: simpleRoleCollection.models});
-            roleListView.render();
         }
-    )
-    ;
+    });
+
+    var roleListView = new RoleListView({model: simpleRoleCollection.models});
+    roleListView.render();
 }
